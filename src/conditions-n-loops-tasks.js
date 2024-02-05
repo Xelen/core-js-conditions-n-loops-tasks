64ef1955a0546fd7d964322db162c99a72084c86
Xuanita <alabama10@inbox.lv>
2024-02-05 04:20:10 +0200

    feat: add canQueenCaptureKing solution

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -66,8 +66,12 @@ function getMaxNumber(a, b, c) {
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  */
-function canQueenCaptureKing(/* queen, king */) {
-  throw new Error('Not implemented');
+function canQueenCaptureKing(queen, king) {
+  const isSameRow = queen.y === king.y;
+  const isSameColumn = queen.x === king.x;
+  const isDiagonal = Math.abs(queen.x - king.x) === Math.abs(queen.y - king.y);
+
+  return isSameRow || isSameColumn || isDiagonal;
 }
 
 /**
